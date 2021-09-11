a7e02885bce7f69870f49ab78d6e37a7b29f49b4
unknown <joks026@gmail.com>
2021-09-11 23:28:03 +0800

    updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>23 - Toggling elements with a key</title>
+        <title>24 - Categorizing lists</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
