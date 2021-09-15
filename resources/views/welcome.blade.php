18a0a7b992fadc3ef5b9de7a064b8314a1a90e89
unknown <joks026@gmail.com>
2021-09-15 14:05:55 +0800

    updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>28 - Using props</title> 
+        <title>29 - Prop options</title> 
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
