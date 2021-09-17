1b95267905c832da5cff20244282f577882e286c
unknown <joks026@gmail.com>
2021-09-17 23:44:12 +0800

    updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>31 - Emitting events from within components</title> 
+        <title>32 - Installing projects using the Vue CLI</title> 
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
