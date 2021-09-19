ef633bd8981e2791dc0a1ca645e4a80ef0074b90
unknown <joks026@gmail.com>
2021-09-19 23:34:12 +0800

    updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>33 - Understanding Vue CLI installations</title> 
+        <title>34 - How CLI components load</title> 
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
