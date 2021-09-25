c9465a945f6823931288bf812184cb5f3c6db2b8
unknown <joks026@gmail.com>
2021-09-25 20:29:27 +0800

    updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>37 - Creating a component</title> 
+        <title>38 - Managing complex child components</title> 
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
